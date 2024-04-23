ef3ee06c50bf2c2cef80f69d9007b584366fcb11
ggreen <gregoryg@vmware.com>
2024-04-23 14:58:14 -0400

    adding topic routing

--- applications/dotnet/Receive/Receive.cs
+++ applications/dotnet/Receive/Receive.cs
@@ -2,14 +2,11 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using Imani.Solutions.Core.API.Util;
-using System.Data.Common;
 
-var factory = new ConnectionFactory { Uri = new Uri("amqp://user:bitnami@localhost"), ClientProvidedName= "Receive"};
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
 
+//Get Settings
 var config = new ConfigSettings();
-
+var clientName = config.GetProperty("clientName","Receive");
 var queueName =  config.GetProperty("queue","hello-quorum");
 var exchangeName = config.GetProperty("exchange","");
 var queueType = config.GetProperty("queueType","quorum");
@@ -18,6 +15,13 @@ uint prefetchSize  = Convert.ToUInt32(config.GetPropertyInteger("prefetchSize",0
 ushort prefetchCount  = Convert.ToUInt16(config.GetPropertyInteger("prefetchCount",1));
 bool autoAckFlag = config.GetPropertyBoolean("autoAck",true);
 
+//Make connection
+
+var factory = new ConnectionFactory { Uri = new Uri("amqp://user:bitnami@localhost"), ClientProvidedName= clientName};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+//Declare Queue
 channel.QueueDeclare(queue: queueName,
                      durable: true,
                      exclusive: false,
@@ -26,15 +30,19 @@ channel.QueueDeclare(queue: queueName,
                      new Dictionary<string, object>()
                         {{ "x-queue-type", queueType}});
 
+//Declare Qualify of service consumption
 channel.BasicQos(prefetchSize: prefetchSize, prefetchCount: prefetchCount, global: false);
 
+//Default Exchange Binding rules
 if(exchangeName.Length > 0 )
     channel.QueueBind(queue: queueName,
                   exchange: exchangeName,
                   routingKey: routingKeyValue);
 
+
 Console.WriteLine(" [*] Waiting for messages.");
 
+//Define Consumer logic
 var consumer = new EventingBasicConsumer(channel);
 consumer.Received += (model, ea) =>
 {
@@ -46,10 +54,11 @@ consumer.Received += (model, ea) =>
         channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
 
 };
-
 channel.BasicConsume(queue: queueName,
                      autoAck: autoAckFlag,
                      consumer: consumer);
+
+
 
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
--- applications/dotnet/Send/Send.cs
+++ applications/dotnet/Send/Send.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 using Imani.Solutions.Core.API.Util;
 using RabbitMQ.Client;
 
 
-var factory = new ConnectionFactory { Uri = new Uri("amqp://user:bitnami@localhost"), ClientProvidedName = "Send"};
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
-
 var config = new ConfigSettings();
+var clientName = config.GetProperty("clientName","Send");
 var routingKeyValue = config.GetProperty("routingKey","hello-quorum");
 var exchangeName = config.GetProperty("exchange","");
 var message = config.GetProperty("message","Hello World!");
 var body = Encoding.UTF8.GetBytes(message);
+
+
+var factory = new ConnectionFactory { Uri = new Uri("amqp://user:bitnami@localhost"), 
+ClientProvidedName = clientName};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
 
 channel.BasicPublish(exchange: exchangeName,
                      routingKey: routingKeyValue,
